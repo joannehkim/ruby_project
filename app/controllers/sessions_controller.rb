175efe4289f5a682a77dd968328980668b8cc50c
kevinyk <kevinyoungkim@gmail.com>
2016-06-04 18:42:18 -0700

    changed the create method in the sessions controller (login) to redirect to previous page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   	@user = User.find_by(email: params[:email])
   	if @user.authenticate(params[:password])
   		session[:user_id] = @user.id 
-  		redirect_to "/users/#{@user.id}"
+  		redirect_to :back
   	else
   		flash[:errors] = ["Invalid email or password."]
   		redirect_to :back
